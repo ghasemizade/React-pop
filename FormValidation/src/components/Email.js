1f43152b116d781d3ccbdbd49414485bc39115a9
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-15 15:53:53 +0330

    fix (prop): user props in email comp

--- FormValidation/src/components/Email.js
+++ FormValidation/src/components/Email.js
@@ -1,11 +1,11 @@
 
 
-export default function Email() {
+export default function Email({value, onChange}) {
     return (
-        <input placeholder="Email" type="email" style={{
+        <input value={value} placeholder="Email" type="email" style={{
             margin: "20px",
             padding: "10px",
             background: "#eee"
-        }}></input>
+        }} onChange={onChange}></input>
     )
 }
