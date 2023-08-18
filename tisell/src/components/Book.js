5d338c330337e8a6318b5677e16097a4582d49df
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-18 21:10:20 +0330

    set firstname and last name

--- tisell/src/components/Book.js
+++ tisell/src/components/Book.js
@@ -6,28 +6,54 @@ import Input from './InputSearch'
 export default function Book() {
     
     return(
-        <main className={clsx(
+        <div className={clsx(
+            "flex justify-center items-center flex-col"
+        )}>
+            <main className={clsx(
             "flex justify-center items-center flex-col",
             "bg-[#30303036]",
             "w-96 h-96",
             "rounded",
+            "rounded-b-none",
             "backdrop-blur"
         )}>
             <Radio />
-            <Input placeholder={'Leaving from...'} label={"leave"}/>
-            <Input placeholder={'Going to...'} label={"go"}/>
+            <Input 
+                placeholder={'Leaving from...'} 
+                label={"leave"}
+                icon={"./location.png"}/>
+            <Input 
+                placeholder={'Going to...'}
+                label={"go"}
+                icon={"./location.png"}/>
             <div className={clsx(
                 "flex justify-center items-center",
                 "px-5"
             )}>
-                <Input placeholder={'First name'} label={"firstname"}/>
+                <Input 
+                    placeholder={'First name'}
+                    label={"firstname"}
+                    icon={"./name.png"}/>
 
                 <gap className={clsx(
                     "mx-0.5"
                 )}></gap>
 
-                <Input placeholder={'Last name'} label={"lastname"}/>
+                <Input 
+                    placeholder={'Last name'}
+                    label={"lastname"}
+                    icon={"./name.png"}/>
             </div>
         </main>
+        <footer>
+            <button className={clsx(
+                "bg-blue-600",
+                "w-96",
+                "rounded-b-md",
+                "py-4",
+                "text-white text-l"
+            )}>SEARCH FLIGHTS</button>
+        </footer>
+        </div>
     )
 }
--- tisell/src/components/InputSearch.js
+++ tisell/src/components/InputSearch.js
@@ -1,7 +1,7 @@
 import clsx from 'clsx'
 import { useRef } from 'react'
 
-export default function InputSearch({placeholder, label}) {
+export default function InputSearch({placeholder, label, icon}) {
     const ref = useRef(null)
     const addClass = () =>{
         const section = ref.current
@@ -26,11 +26,9 @@ export default function InputSearch({placeholder, label}) {
                 "duration-100"
             )} onFocus={addClass} onBlur={removeClass} ref={ref}>
                 <label htmlFor={label}>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className={clsx(
-                        "w-8 h-8 mx-3 text-gray-500"
-                    )} fill="currentColor">
-                        <path d="M18,4.48a8.45,8.45,0,0,0-12,12l5.27,5.28a1,1,0,0,0,1.42,0L18,16.43A8.45,8.45,0,0,0,18,4.48ZM16.57,15,12,19.59,7.43,15a6.46,6.46,0,1,1,9.14,0ZM9,7.41a4.32,4.32,0,0,0,0,6.1,4.31,4.31,0,0,0,7.36-3,4.24,4.24,0,0,0-1.26-3.05A4.3,4.3,0,0,0,9,7.41Zm4.69,4.68a2.33,2.33,0,1,1,.67-1.63A2.33,2.33,0,0,1,13.64,12.09Z"/>
-                    </svg>
+                    <img className={clsx(
+                        "w-6 h-6 mr-4 ml-2"
+                    )} src={icon} alt={icon} />
                 </label>
                 <input className={clsx(
                     "bg-slate-200",
