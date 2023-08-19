b1df7f1719c4bb7cbe7696d1d69ffd87ea9f9140
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-19 13:46:18 +0330

    set app structure

--- tisell/src/App.js
+++ tisell/src/App.js
@@ -1,18 +1,24 @@
 import './App.css';
 import clsx from 'clsx'
 import Book from './components/Book';
+import Decs from './components/Decs';
 
 function App() {
   return (
     <div className={clsx(
       "flex justify-center items-center",
-      "min-h-screen"
+      "min-h-screen",
+        "bg-[#30303036]",
+        "backdrop-blur",
+      )}>
+      <div className={clsx(
+        "container",
+        "flex justify-between items-center",
+        "px-40"
     )}>
-
+      <Decs />
       <Book />
-
-
-
+    </div>
     </div>
   );
 }
