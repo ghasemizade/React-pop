6acb0e0614221a173e61caa9b9acb3c32590e62d
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-20 15:47:14 +0330

    fix (set icons comp)

--- booklist/src/components/Main.js
+++ booklist/src/components/Main.js
@@ -1,10 +1,28 @@
 import Inputbookdetails from "./Inputbookdetails";
-
+import { MdTitle } from 'react-icons/md'
+import { BsFillPersonFill, BsCalendarDateFill } from 'react-icons/bs'
+import clsx from "clsx";
 
 export default function Main(){
     return (
         <>
-            <Inputbookdetails label={"title"}/>
+            <Inputbookdetails label={"Title"}>
+                <MdTitle className={clsx(
+                    "mr-2"
+                )} />
+            </Inputbookdetails>
+
+            <Inputbookdetails label={"Author"}>
+                <BsFillPersonFill className={clsx(
+                    "mr-2"
+                )} />
+            </Inputbookdetails>
+
+            <Inputbookdetails label={"Year"}>
+                <BsCalendarDateFill className={clsx(
+                    "mr-2"
+                )} />
+            </Inputbookdetails>
         </>
     )
 }
