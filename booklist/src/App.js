908ab537dff37dcd4a6c1b469959c1ca05767eca
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-21 00:50:46 +0330

    fix (props & func): add function for handling input and set props to main comp

--- booklist/src/App.js
+++ booklist/src/App.js
@@ -1,12 +1,43 @@
 import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
+import Stashbook from './components/Stashbook';
+import { useState } from 'react'
+
 
 function App() {
+  const [nameValue, setNameValue] = useState('')
+  const [authorValue, setAuthorValue] = useState('')
+  const [yearValue, setYearValue] = useState('')
+
+  function changeName(event){
+      setNameValue(event.target.value)
+  }
+
+  function changeAuthor(event){
+    setAuthorValue(event.target.value)
+}
+
+function changeYear(event){
+  setYearValue(event.target.value)
+}
+
+function changeButton(event){
+
+}
+
   return (
     <>
       <Header/>
-      <Main/>
+      <Main 
+        changehandler={changeButton}
+        changehandlername={changeName}
+        changehandlerauthor={changeAuthor}
+        changehandleryear={changeYear}/>
+      <p>{nameValue}</p>
+      <p>{authorValue}</p>
+      <p>{yearValue}</p>
+      <Stashbook/>
     </>
   );
 }
