fa43786f6d8e8dc779d9456298d5f097eee121d2
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-22 13:46:36 +0330

    import card comp

--- ShoppingCart/src/components/Main.js
+++ ShoppingCart/src/components/Main.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
 import './Main.css'
+import Card from './Card'
 
 export default class Main extends Component {
   render() {
     return (
       <main>
-        main
+        <Card/>
       </main>
     )
   }
