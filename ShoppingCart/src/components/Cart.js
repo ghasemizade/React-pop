a4e2eb5ac8fe052241b67cc57113d5fd873a4aaa
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-22 17:50:44 +0330

    set clickhandler to on click cart

--- ShoppingCart/src/components/Cart.js
+++ ShoppingCart/src/components/Cart.js
@@ -4,15 +4,19 @@ import './Cart.css'
 
 export default class Cart extends Component{
 
+    clickHandler (id){
+        this.props.onRemoveItem(id)
+    }
+
     render(){
-        let {title, price, src} = this.props
+        let {id, title, price, src} = this.props
 
         return (
             <div className='cart'>
                 <h2>{title}</h2>
                 <p>{price}</p>
                 <Img src={src} width={60} height={60}/>
-                <button className="cancle">
+                <button className="cancle" onClick={this.clickHandler.bind(this, id)}>
                     Cancle
                 </button>
         </div>
