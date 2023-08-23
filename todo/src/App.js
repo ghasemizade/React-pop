a6b61190019f788b39b9551cf3cfbcb72145a520
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-23 16:49:47 +0330

    upd files

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,19 +1,12 @@
-import clsx from 'clsx';
 import Header from './components/Header';
 import Main from './components/Main';
-import Todo from './components/Todo';
 
 function App() {
   return (
     <>
       <Header />
       <Main />
-      <div className={clsx(
-        "flex justify-center items-center",
-        "mt-20"
-      )}>
-        <Todo />
-      </div>
+      
     </>
   );
 }
