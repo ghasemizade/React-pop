0e79ed0502d3283deb365b04ee0128cc9e96aaeb
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-23 16:50:54 +0330

    set method

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -1,23 +1,50 @@
 import clsx from 'clsx'
 import React, { Component } from 'react'
 import { TiTick, TiTrash } from 'react-icons/ti'
+import '../index.css'
 
 export default class Todo extends Component {
-  render() {
+
+    removeHandler (id){
+      this.props.onRemove(id)
+    }
+
+    completed (id){
+      this.props.onCompleted(id)
+    }
+
+    render() {
     return (
       <div className={clsx(
+        `todo ${this.props.completed ? 'todo-title' : ''}`,
         "flex justify-between items-center",
         "bg-white",
         "w-80",
+        "rounded-lg",
+        "mt-3",
       )}>
         <p className={clsx(
-            "text-left"
-        )}>todo</p>
+            "ml-2",
+        )}>{this.props.title}</p>
         <div className={clsx(
-            "flex justify-center items-center"
+            "flex justify-center items-center",
         )}>
-            <TiTick size={30} color='#6366f1' cursor={'pointer'}/>
-            <TiTrash size={30} color='#6366f1' cursor={'pointer'}/>
+            <TiTick
+                className={clsx("bg-green-300", "py-1")} 
+                size={30} 
+                color='#6366f1' 
+                cursor={'pointer'}
+                onClick={this.completed.bind(this, this.props.id)}
+                />
+            <TiTrash
+                className={clsx(
+                    "bg-red-300",
+                    "rounded-r-lg"
+                    )} 
+                size={30} 
+                color='#6366f1' 
+                cursor={'pointer'}
+                onClick={this.removeHandler.bind(this, this.props.id)}/>
         </div>
       </div>
     )
