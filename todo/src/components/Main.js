a7643e1411a2b6d6c33d91c627bb59aaf20753e8
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-23 13:55:00 +0330

    feat (main comp)

---
+++ todo/src/components/Main.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import Input from './Input'
+import clsx from 'clsx'
+import Filter from './Filter'
+
+export default class Main extends Component {
+  render() {
+    return (
+      <main className={clsx(
+        "flex justify-center items-center",
+        "mt-40",
+      )}>
+        <div className={clsx(
+            "flex justify-center items-center"
+        )}>
+            <Input />
+            <Filter />
+        </div>
+      </main>
+    )
+  }
+}
