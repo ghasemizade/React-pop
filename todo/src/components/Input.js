a757bc752cc03724f43e7f5fd749f9467042613d
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-23 16:50:01 +0330

    write functionality

--- todo/src/components/Input.js
+++ todo/src/components/Input.js
@@ -1,33 +1,109 @@
 import clsx from 'clsx'
 import React, { Component } from 'react'
 import {AiFillPlusCircle} from 'react-icons/ai'
+import Todo from './Todo';
+import Filter from './Filter';
 
 export default class Input extends Component {
+    constructor(props) {
+      super(props);
+      
+      this.state = {
+          todos: [],
+          todoTitle: '',
+          status: 'all'
+      }
 
-    clickHandler(event){
-        console.log(event);
+      this.changeHandler = this.changeHandler.bind(this)
+      this.clickHandler = this.clickHandler.bind(this)
+      this.removetodo = this.removetodo.bind(this)
+      this.completed = this.completed.bind(this)
     }
+    
+    changeHandler(event){
+      this.setState({
+        todoTitle: event.target.value
+      })
+    }
+
+    clickHandler(){
+        let newTodo = {
+          id: this.state.todos.length + 1,
+          title: this.state.todoTitle,
+          completed: false
+        }
+
+        this.setState(preveState =>{
+          return {
+            todos: [...preveState.todos, newTodo],
+            todoTitle: ''
+          }
+        })
+    }
+
+    removetodo (todoId){
+
+      let newtodos = this.state.todos.filter(todo => {
+        return todo.id !== todoId
+      })
+      
+      this.setState({
+        todos: newtodos
+      })
+    }
+
+    completed (todoId){
+      let newTodos = [...this.state.todos]
+
+
+      newTodos.forEach(todo => {
+        if (todo.id === todoId) {
+          todo.completed = !todo.completed
+        }
+      })
+      this.setState({
+        todos: newTodos
+      })
+    }
+
   render() {
     return (
-      <div className={clsx(
-        "flex justify-center items-center",
-        "bg-white",
-        "pr-2",
-        "rounded-lg",
-        "mr-10"
-      )}>
-        <input className={clsx(
-            "outline-none",
-            "px-5 py-1",
-            "w-full",
+      <div>
+        <div className={clsx(
+          "flex justify-center items-center"
+        )}>
+          <div className={clsx(
+            "flex justify-center items-center",
             "bg-white",
+            "pr-2",
             "rounded-lg",
-        )} type='text' placeholder='Todo...'/>
-        <AiFillPlusCircle 
-            size={20} 
-            color='#6366f1' 
-            cursor={'pointer'} 
-            onClick={this.clickHandler}/>
+            "mr-10"
+          )}>
+            <input className={clsx(
+                "outline-none",
+                "px-5 py-1",
+                "w-full",
+                "bg-white",
+                "rounded-lg",
+            )} type='text' placeholder='Todo...' 
+                value={this.state.todoTitle}
+                onChange={this.changeHandler} />
+            <AiFillPlusCircle 
+                size={20} 
+                color='#6366f1' 
+                cursor={'pointer'} 
+                onClick={this.clickHandler}/>
+          </div>
+          <Filter/>
+        </div>
+        <div className={clsx(
+          "flex justify-center items-center flex-col",
+          "mt-20"
+          )}>
+            {this.state.todos.map(todo => (
+              <Todo key={todo.id} {...todo} onRemove={this.removetodo} onCompleted={this.completed}/>
+            ))}
+        </div>
       </div>
     )
   }
