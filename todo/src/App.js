d0558e4ee3f090fcced8d8bc3ce3c0532ceb6e51
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-23 13:53:51 +0330

    import (components): Header, Main, Todo imported

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import clsx from 'clsx';
+import Header from './components/Header';
+import Main from './components/Main';
+import Todo from './components/Todo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Main />
+      <div className={clsx(
+        "flex justify-center items-center",
+        "mt-20"
+      )}>
+        <Todo />
+      </div>
+    </>
   );
 }
 
