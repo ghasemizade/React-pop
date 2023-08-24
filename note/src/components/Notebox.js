468c33799d69c5457801aec3eb2bba7ea51e71cc
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-24 14:41:04 +0330

    set props

--- note/src/components/Notebox.js
+++ note/src/components/Notebox.js
@@ -3,17 +3,16 @@ import clsx from 'clsx'
 
 export default class Notebox extends Component {
   render() {
-    let {noteTitle, color} = this.props
     return (
       <div className={clsx(
         "mt-20",
         "bg-white",
         "px-10 py-2 m-5",
         "rounded-lg"
-      )}>{noteTitle}
+      )}>
         <h1 className={clsx(
             "text-3xl",
-        )} style={{color: {color}}}>test</h1>
+        )} style={{color: this.props.color}}>{this.props.title}</h1>
       </div>
     )
   }
