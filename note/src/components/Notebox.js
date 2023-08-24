1d058a0563cefbb70aabf4cb967be6527c698ecd
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-24 15:33:55 +0330

    set click event to remove

--- note/src/components/Notebox.js
+++ note/src/components/Notebox.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react'
 import clsx from 'clsx'
 
 export default class Notebox extends Component {
+
+  clickHandler(id){
+    this.props.onRemove(id)
+  }
+
   render() {
     return (
       <div className={clsx(
@@ -9,7 +14,7 @@ export default class Notebox extends Component {
         "bg-white",
         "px-10 py-2 m-5",
         "rounded-lg"
-      )}>
+      )} onClick={this.clickHandler.bind(this, this.props.id)}>
         <h1 className={clsx(
             "text-3xl",
         )} style={{color: this.props.color}}>{this.props.title}</h1>
