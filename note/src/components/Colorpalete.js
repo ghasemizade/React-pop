ce98a46bf9296b155b97495c833bfc9611fad3e2
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-24 14:30:20 +0330

    set event onClick

--- note/src/components/Colorpalete.js
+++ note/src/components/Colorpalete.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react'
 import clsx from 'clsx'
 
 export default class Palete extends Component {
+
+  colorClick (color){
+    this.props.onColor(color)
+  }
+
   render() {
     let {color} = this.props
     return (
@@ -9,7 +14,7 @@ export default class Palete extends Component {
             "border-2",
             "rounded-full",
             "w-7 h-7"
-        )} style={{backgroundColor: `${color}`}}></div>
+        )} style={{backgroundColor: `${color}`}} onClick={this.colorClick.bind(this, this.props.color)}></div>
 
     )
   }
