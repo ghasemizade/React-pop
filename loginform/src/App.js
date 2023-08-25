413982ebc6a455a73f6c4ec82097a54e69db4dcc
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-25 06:26:40 +0330

    create structure with bootstrap

--- loginform/src/App.js
+++ loginform/src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
-
+import { Container, Row, Image, Col } from 'react-bootstrap'
+import Menu from './components/Menus';
+import Cards from './components/Cards';
 
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Container fluid >
+      <Row>
+        <Menu/>
+      </Row>
+      <Row>
+        <Image src='./banner.jpg' thumbnail rounded style={{
+          border: "none",
+          margin: "0",
+          padding: "0"
+          }}></Image>
+      </Row>
+      <Row>
+        <Col style={{
+          marginTop: "30px",
+          margin: "30px 180px"
+        }}>
+          <Cards/>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
