6a37a2bcb48f172fb5635dac6cf8613ed29d1a83
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-28 11:49:49 +0330

    refactor footer

--- ShoppingCart/src/components/Footer.js
+++ ShoppingCart/src/components/Footer.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './Footer.css'
 
-export default class Footer extends Component {
-  render() {
-    return (
-      <footer>
-        <div className='contact'>
-            <a className='telegram' href='https://t.me/Hosein_ghasemizade'>
-                TELEGRAM
-            </a>
-            <a href="https://ghasemizade.com">
-                PERSONAL WEBSITE
-            </a>
-            <a className='github' href='https://github.com/ghasemizade'>
-                GITHUB
-            </a>
-        </div>
-      </footer>
-    )
-  }
+export default function Footer () {
+
+  return (
+    <footer>
+      <div className='contact'>
+          <a className='telegram' href='https://t.me/Hosein_ghasemizade'>
+              TELEGRAM
+          </a>
+          <a href="https://ghasemizade.com">
+              PERSONAL WEBSITE
+          </a>
+          <a className='github' href='https://github.com/ghasemizade'>
+              GITHUB
+          </a>
+      </div>
+    </footer>
+  )  
 }
