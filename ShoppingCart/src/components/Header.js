2f775ba22cde3d699a18e3486b4152ab68117884
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-28 11:49:04 +0330

    update header from class comp to func comp

--- ShoppingCart/src/components/Header.js
+++ ShoppingCart/src/components/Header.js
@@ -2,18 +2,17 @@ import React, { Component } from 'react'
 import './Header.css'
 
 
-export default class Header extends Component {
-  render() {
-    return (
-      <header>
-        <nav className='nav-bar'>
-            <ul>
-                <li className='home'>Home</li>
-                <li className='store'>Store</li>
-                <li className='about'>About</li>
-            </ul>
-        </nav>
-      </header>
-    )
-  }
+export default function Header () {
+
+  return (
+    <header>
+      <nav className='nav-bar'>
+          <ul>
+              <li className='home'>Home</li>
+              <li className='store'>Store</li>
+              <li className='about'>About</li>
+          </ul>
+      </nav>
+    </header>
+  )
 }
