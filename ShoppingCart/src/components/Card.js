414df24b6a251605dae72a663d9ff3efbaf06e05
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-28 12:02:52 +0330

    refactoring card comp, class to func

--- ShoppingCart/src/components/Card.js
+++ ShoppingCart/src/components/Card.js
@@ -1,92 +1,73 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Img from './Img'
 import './Card.css'
 import Cart from './Cart';
 
-export default class Card extends Component {
+export default function Card() {
+    const [products] = useState(
+      [
+        {id: 1, title: "Jordan", price: "99$", src: "/shoe1.png"},
+        {id: 2, title: "Jordan Air", price: "89$", src: "/shoe2.png"},
+        {id: 3, title: "Jordan Jump", price: "78$", src: "/shoe3.png"},
+        {id: 4, title: "Jordan Air2", price: "64$", src: "/shoe4.png"},
+        {id: 5, title: "Jordan Classic", price: "85$", src: "/shoe5.png"},
+        {id: 6, title: "Jordan Dj khaled", price: "85$", src: "/shoe6.png"},
+        {id: 7, title: "Jordan Man", price: "85$", src: "/shoe7.png"},
+        {id: 8, title: "Jordan Sport", price: "85$", src: "/shoe8.png"},
+        {id: 9, title: "Jordan Jump2", price: "85$", src: "/shoe9.png"},
+        {id: 10, title: "Jordan Air Man", price: "85$", src: "/shoe10.png"},
+        {id: 11, title: "Jordan Classic2", price: "85$", src: "/shoe11.png"},
+    ],
+    )
+    const [shoppingCart, setShoppingCart] = useState([])
 
-    constructor(props) {
-        super(props);
-        
-        this.state = {
-            products: [
-                {id: 1, title: "Jordan", price: "99$", src: "/shoe1.png"},
-                {id: 2, title: "Jordan Air", price: "89$", src: "/shoe2.png"},
-                {id: 3, title: "Jordan Jump", price: "78$", src: "/shoe3.png"},
-                {id: 4, title: "Jordan Air2", price: "64$", src: "/shoe4.png"},
-                {id: 5, title: "Jordan Classic", price: "85$", src: "/shoe5.png"},
-                {id: 6, title: "Jordan Dj khaled", price: "85$", src: "/shoe6.png"},
-                {id: 7, title: "Jordan Man", price: "85$", src: "/shoe7.png"},
-                {id: 8, title: "Jordan Sport", price: "85$", src: "/shoe8.png"},
-                {id: 9, title: "Jordan Jump2", price: "85$", src: "/shoe9.png"},
-                {id: 10, title: "Jordan Air Man", price: "85$", src: "/shoe10.png"},
-                {id: 11, title: "Jordan Classic2", price: "85$", src: "/shoe11.png"},
-            ],
-            shoppingCart: []
-        }
-
-        this.clickHandler = this.clickHandler.bind(this)
-        this.emptyShoppingCart = this.emptyShoppingCart.bind(this)
-        this.removeItemCart = this.removeItemCart.bind(this)
-      }
-
-      clickHandler(productId){
-        let mainProduct = this.state.products.find(product => {
+      const clickHandler = (productId) =>{
+        let mainProduct = products.find(product => {
           return product.id === productId
         })
-        console.log(mainProduct);
-      
-        this.setState(prevState => {
-          return {
-            shoppingCart: [...prevState.shoppingCart, mainProduct]
-          }
-        })
+
+        setShoppingCart(prevState => [...prevState.shoppingCart, mainProduct])
       }
     
-      emptyShoppingCart (){
-        this.setState({
-          shoppingCart: []
-        })
+      const emptyShoppingCart = () =>{
+        setShoppingCart([])
       }
 
-      removeItemCart (productId){
-        let newShoppingCart = this.state.shoppingCart.filter(product => {
+      const removeItemCart = (productId) =>{
+        let newShoppingCart = shoppingCart.filter(product => {
           return product.id !== productId
         })
 
-        this.setState ({
-          shoppingCart: newShoppingCart
-        })
+        setShoppingCart(newShoppingCart)
       }
 
-  render() {
-    return (
-      <div className='container'>
-        <div className='card-items'>
-            {this.state.products.map((product) => (
-                    <div className='card'>
-                      <h2>{product.title}</h2>
-                      <div className='image-card'>
-                        <Img {...product} width={300} height={300}/>
-                      </div>
-                      <div className='detail'>
-                        <p>{product.price}</p>
-                        <button onClick={this.clickHandler.bind(this, product.id)}>
-                          ADDTOCART
-                        </button>
-                      </div>
+  return (
+    <div className='container'>
+      <div className='card-items'>
+          {products.map((product) => (
+                  <div className='card'>
+                    <h2>{product.title}</h2>
+                    <div className='image-card'>
+                      <Img {...product} width={300} height={300}/>
                     </div>
-            ))}      
-        </div>
-        <div className="cart-items">
-            {this.state.shoppingCart.map(product => (
-              <Cart {...product} onRemoveItem={this.removeItemCart}/>
-            ))}
-        </div>
-        <div className='reset' onClick={this.emptyShoppingCart}>
-          <button>RESET</button>
-        </div>
+                    <div className='detail'>
+                      <p>{product.price}</p>
+                      <button onClick={() => clickHandler(product.id)}>
+                        ADDTOCART
+                      </button>
+                    </div>
+                  </div>
+          ))}      
       </div>
-    )
-  }
+      <div className="cart-items">
+          {shoppingCart.map(product => (
+            <Cart {...product} onRemoveItem={removeItemCart}/>
+          ))}
+      </div>
+      <div className='reset' onClick={emptyShoppingCart}>
+        <button>RESET</button>
+      </div>
+    </div>
+  )
+
 }
