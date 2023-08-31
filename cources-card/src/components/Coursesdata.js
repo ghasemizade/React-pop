a3f34dd52d84e2e4ab42fbefbd8964f941130a15
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-31 19:30:36 +0330

    coursesdata file

---
+++ cources-card/src/components/Coursesdata.js
@@ -0,0 +1,11 @@
+let cources = [
+    {id: 1, name: 'html', desc: '', imgsrc: './html.png'},
+    {id: 2, name: 'css', desc: '', imgsrc: '/css.png'},
+    {id: 3, name: 'js', desc: '', imgsrc: '/js.png'},
+    {id: 4, name: 'python', desc: '', imgsrc: '/python.png'},
+    {id: 5, name: 'react', desc: '', imgsrc: '/logo192.png'},
+    {id: 6, name: 'flutter', desc: '', imgsrc: '/flutter.png'},
+]
+
+
+export default cources
