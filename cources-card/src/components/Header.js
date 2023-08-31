76468a7b48ed2f8adb20bebd66de06282317eaf7
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-08-31 19:30:44 +0330

    header comp

---
+++ cources-card/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../index.css'
+
+export default function Header() {
+  return (
+    <header>
+        <h1 className='flex justify-center items-center uppercase font-black text-4xl mt-20 tracking-widest'>
+            courses
+        </h1>
+    </header>
+  )
+}
