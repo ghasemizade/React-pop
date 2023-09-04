111a8360f72dfc239f0fd78ae491413247a94017
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-05 00:07:45 +0330

    redirect 404 page

--- spa-chalange/src/components/Mainproduct.js
+++ spa-chalange/src/components/Mainproduct.js
@@ -1,13 +1,26 @@
 import React, { useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Navigate } from 'react-router-dom'
 import productData from '../utils'
 
 export default function Mainproduct() {
     let params = useParams()
     const [products] = useState(productData)
-    console.log(params);
+    
+    let hasProduct = products.some(product => product.id == params.productId)
 
   return (
-    <div>Mainproduct: {products.find(product => product.id == params.productId).title}</div>
+    <div>
+        {!hasProduct ? (
+            <h1>
+                Not Find Product
+            </h1>
+        ) : (
+            <>
+                Mainproduct: {products.find(product => product.id == params.productId).title}
+            </>
+        ) 
+
+    }
+    </div>
   )
 }
