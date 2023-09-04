869d9a40bf13a52be4c86d8b8f1d79af21301a61
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-04 12:40:06 +0330

    product js

--- spa-chalange/src/components/Product.js
+++ spa-chalange/src/components/Product.js
@@ -1,8 +1,18 @@
-import React from 'react'
-
+import React, {useState} from 'react'
+import productData from '../utils'
+import { Link } from 'react-router-dom'
 
 export default function Product() {
-  return (
-    <div>ProductPage</div>
+  const [products] = useState(productData)
+  
+    return (
+    <div>
+        {products.map(product =>(
+            <React.Fragment key={product.id}>
+                <Link to={`/product/${product.id}`}>{product.title}</Link>
+                <hr/>    
+            </React.Fragment>
+        ))}
+    </div>
   )
 }
