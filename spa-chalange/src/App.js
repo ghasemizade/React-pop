27ecd5e806bda556f63e6f8780c72c79e973e0d7
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-05 16:05:20 +0330

    useRoutes hook

--- spa-chalange/src/App.js
+++ spa-chalange/src/App.js
@@ -1,37 +1,43 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useRoutes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import Contact from './components/Contact';
-import About from './components/About';
+import About from './components/About/About';
 import Product from './components/Product';
 import Mainproduct from './components/Mainproduct';
 
 
 function App() {
+
+  let router = useRoutes([
+    { 
+      path:"/",
+      element:<Home/>
+    },
+    {
+      path:"/product",
+      element:<Product/>},
+    {
+      path:"/product/:productId",
+      element:<Mainproduct/>
+    },
+    {
+      path:"/contact",
+      element:<Contact/>
+    },
+    {
+      path:"/about/*",
+      element:<About/>,
+      children: [
+        {path:"setting", element: <span> {`>`} setting</span>},
+        {path:"dashboard", element: <span> {`>`} dashboard</span>},
+      ]
+    },
+  ])
+
   return (
     <>
-      <Routes>
-
-        <Route 
-          path="/"
-          element={<Home/>} />
-          
-        <Route 
-          path="/product"
-          element={<Product/>} />
-        <Route 
-          path="/product/:productId"
-          element={<Mainproduct/>} />
-
-        <Route 
-          path="/contact"
-          element={<Contact/>} />
-
-        <Route 
-          path="/about"
-          element={<About/>} />
-
-      </Routes>
+      {router}
     </>
   );
 }
