85c6a37b1a97f875e44062dead6274c06d2ea98e
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-05 18:34:57 +0330

    upd navbar

--- spa-chalange/src/components/Navbar.js
+++ spa-chalange/src/components/Navbar.js
@@ -2,8 +2,12 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom'
+import { isUserLogin } from './../utils'
 
 function ColorSchemesExample() {
+
+  // let isUserLogin = isLogin('ali')
+
   return (
     <>
       <Navbar bg="dark" data-bs-theme="dark">
@@ -30,6 +34,18 @@ function ColorSchemesExample() {
                 textDecoration: "none",
                 color: "#ccc"
             }} to={'/about'}>About</Link>
+            {isUserLogin ? 
+            <Link style={{
+                marginLeft: "10px",
+                textDecoration: "none",
+                color: "green"
+            }} to={'/dashboard'}>dashboard</Link> 
+            : 
+            <Link style={{
+                marginLeft: "10px",
+                textDecoration: "none",
+                color: "tomato"
+            }} to={'/login'}>Login</Link>}
           </Nav>
         </Container>
       </Navbar>
