ef649310402327a9e56946d03212a5efa825aea4
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-05 16:09:42 +0330

    routes save different file

---
+++ spa-chalange/src/routes.js
@@ -0,0 +1,34 @@
+import Home from './components/Home';
+import Contact from './components/Contact';
+import About from './components/About/About';
+import Product from './components/Product';
+import Mainproduct from './components/Mainproduct';
+
+
+let router = [
+    { 
+      path:"/",
+      element:<Home/>
+    },
+    {
+      path:"/product",
+      element:<Product/>},
+    {
+      path:"/product/:productId",
+      element:<Mainproduct/>
+    },
+    {
+      path:"/contact",
+      element:<Contact/>
+    },
+    {
+      path:"/about/*",
+      element:<About/>,
+      children: [
+        {path:"setting", element: <span> {`>`} setting</span>},
+        {path:"dashboard", element: <span> {`>`} dashboard</span>},
+      ]
+    },
+  ]
+
+  export default router
