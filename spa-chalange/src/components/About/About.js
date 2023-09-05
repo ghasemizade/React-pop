8099b7b2a4f99cff35d9db4311ef9471c48e302a
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-05 16:05:51 +0330

    change directory

---
+++ spa-chalange/src/components/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+export default function About() {
+  return (
+    <>
+      <span>AboutPage</span>
+      <Outlet/>
+    </>
+  )
+}
