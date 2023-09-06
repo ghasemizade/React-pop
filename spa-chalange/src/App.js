d04e8d9d151e7028efcc0d7025a545ced96040b4
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-06 12:25:33 +0330

    update

--- spa-chalange/src/App.js
+++ spa-chalange/src/App.js
@@ -2,16 +2,73 @@ import './App.css';
 import { useRoutes } from 'react-router-dom';
 import router from './routes';
 
-
-function App() {
-
-  let routers = useRoutes(router)
-
+const articles = [
+  {
+    id: 1,
+    title: 'Article 1',
+    content: 'This is the content of Article 1.',
+    imageUrl: 'https://example.com/article1.jpg',
+  },
+  {
+    id: 2,
+    title: 'Article 2',
+    content: 'This is the content of Article 2.',
+    imageUrl: 'https://example.com/article2.jpg',
+  },
+  // Add more articles as needed
+];
+// Functional component for the header
+function Header() {
   return (
-    <>
-      {routers}
-    </>
+    <header className="header">
+      <div className="logo">
+        <h1>Your Website Name</h1>
+      </div>
+      <nav className="nav">
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
   );
 }
+
+// Functional component for the main content
+function Main() {
+  return (
+    <main>
+      <section>
+        <h2>Welcome to Your Website</h2>
+        <p>This is the main content area of your website.</p>
+      </section>
+    </main>
+  );
+}
+
+// Functional component for the footer
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; {new Date().getFullYear()} Your Website Name</p>
+    </footer>
+  );
+}
+
+// Functional component for the entire site
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+    // <>
+    //   {routers}
+    // </>
 
 export default App;
