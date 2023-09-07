72f39bbc7a66b9b04fded496a88b4a22e29e01a5
Hosein Ghasemizade <92257857+ghasemizade@users.noreply.github.com>
2023-09-07 05:55:51 +0330

    update

--- firebase-test/src/utils/regex.tsx
+++
@@ -1,9 +0,0 @@
-const regex = ({
-    number: /^(?=.*[0-9]).+$/,
-    lowerCase: /^(?=.*[a-z]).+$/,
-    upperCase: /^(?=.*[A-Z]).+$/,
-    specialCharacter: /^(?=.*[$&+,:;=?@#|'`~<>_{}.^*()%!]).+$/,
-
-})
-
-export default regex
